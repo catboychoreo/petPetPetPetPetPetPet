d60e45bc258777a47bccfbedf91290b5656da14e
catboychoreo <135279465+catboychoreo@users.noreply.github.com>
2024-04-14 20:15:47 -0500

    pet.js
    made drag and drop instead of click...

--- pet.js
+++ pet.js
@@ -5,21 +5,35 @@ var catReaction = document.getElementById("catreact");
 var kitty = document.getElementById("kitty")
 var instructions = document.getElementById("instruction")
 
+var grabbedItem = "grab something..."
 
-foodBowl.onclick = function () {
-    catReaction.innerText = "nom....nom....nom....meoww"
-    kitty.src = "kittyeat.png"
-    instructions.innerText = "your pet is Very Full and Loves Its Food!!!!"
+function allowDrop(ev){
+    ev.preventDefault();
 }
 
-bed.onclick = function () {
-    catReaction.innerText = "snzzzzz..."
-    kitty.src = "kittysleep.png"
-    instructions.innerText = "your pet is Sound Asleep and is dreaming beautiful things....."
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+    console.log(ev.target.id);
+    grabbedItem = ev.target.id
 }
 
-yarn.onclick = function () {
-    catReaction.innerText = "meowww!!!!!!!"
-    kitty.src = "kittyplay.png"
-    instructions.innerText = "your pet is Playing til it drops.. might wanna give it some rest"
+function drop(ev, el) {
+    console.log(ev);
+    ev.preventDefault();
+    var thing = ev.dataTransfer.getData("text");
+    el.appendChild(document.getElementById(thing));
+
+    if (grabbedItem == "food") {
+        catReaction.innerText = "nom....nom....nom....meoww"
+        kitty.src = "kittyeat.png"
+        instructions.innerText = "your pet is Very Full and Loves Its Food!!!!"
+    } else if (grabbedItem == "bed") {
+        catReaction.innerText = "snzzzzz..."
+        kitty.src = "kittysleep.png"
+        instructions.innerText = "your pet is Sound Asleep and is dreaming beautiful things....."
+    } else if (grabbedItem == "yarn") {
+        catReaction.innerText = "meowww!!!!!!!"
+        kitty.src = "kittyplay.png"
+        instructions.innerText = "your pet is Playing til it drops.. might wanna give it some rest"
+    }
 }
